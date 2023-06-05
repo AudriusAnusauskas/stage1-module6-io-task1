fbe1502bca940e4b50101153d11f3c46059f7fdc
AudriusAnusauskas <aaaudrius@gmail.com>
2023-06-05 20:03:28 +0300

    finish java i/o task, stage 1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,67 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
+    private String name;
+    private int age;
+    private String email;
+    private long phone;
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        try(BufferedReader br = new BufferedReader(new java.io.FileReader(file))){;
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] keyValue = line.split(":");
+
+                if (keyValue.length == 2) {
+                    String key = keyValue[0].trim();
+                    String value = keyValue[1].trim();
+
+                    switch (key) {
+                        case "Name":
+                            name = value;
+                            break;
+                        case "Age":
+                            age = Integer.parseInt(value);
+                            break;
+                        case "Email":
+                            email = value;
+                            break;
+                        case "Phone":
+                            phone = Long.parseLong(value);
+                            break;
+                    }
+                }
+            }
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+        return new Profile(getName(), getAge(), getEmail(), getPhone());
     }
-}
+    public String getName(){
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+
+
+
+    }
+//    public static void main(String[] args) {
+//       FileReader fr = new FileReader();
+//       fr.getDataFromFile(new File("C:\\Users\\aaaud_3unkhoj\\Documents\\Audrius\\Epam\\mjc-school\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt"));
+//    }
